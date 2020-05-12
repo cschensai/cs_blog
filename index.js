229b4ac45eeac2afa5dacbc65bd302a13a1ed228
chensai <chensai@chensaideMacBook-Pro.local>
2020-05-12 12:26:21 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var index = (function (_ref) {
+  var title = _ref.title;
+  return React.createElement("h1", null, title, "666");
+});
+
+exports.Foo = index;
