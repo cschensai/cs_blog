23d1f0de87034445461f3f24f8dbbe9392f3690d
chensai <chensai@chensaideMacBook-Pro.local>
2020-05-12 10:58:45 +0800

    fix: 更新依赖

--- .umirc.ts
+++ .umirc.ts
@@ -4,4 +4,6 @@ export default defineConfig({
   mode: 'doc', // 文档模式（默认值） site: 站点模式
   title: 'Library Name',
   // more config: https://d.umijs.org/config
+  publicPath: '/soldier-ui/',
+  base: '/soldier-ui/',
 });
